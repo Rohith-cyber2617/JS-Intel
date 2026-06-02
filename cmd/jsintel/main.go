28210a94f7247ecde54c47cb5f04963465502664
Tony <rohithrachapudi96@gmail.com>
2026-06-02 10:28:22 +0530

    Add help menu support

--- cmd/jsintel/main.go
+++ cmd/jsintel/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/Rohith-cyber2617/JS-Intel/internal/banner"
+	helpmenu "github.com/Rohith-cyber2617/JS-Intel/internal/help"
 )
 
 func main() {
+	helpFlag := flag.Bool("help", false, "Display help menu")
+	helpShort := flag.Bool("h", false, "Display help menu")
+
+	flag.Parse()
+
+	if *helpFlag || *helpShort {
+		fmt.Println(banner.Banner)
+		fmt.Println(helpmenu.HelpMenu)
+		return
+	}
+
 	fmt.Println(banner.Banner)
 }
