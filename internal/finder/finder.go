1f4f101c19b227d564d923b7357fe1c5a6587767
Tony <rohithrachapudi96@gmail.com>
2026-06-02 11:31:29 +0530

    Expand intelligence signatures database

--- internal/finder/finder.go
+++ internal/finder/finder.go
@@ -4,27 +4,96 @@ import (
 	"regexp"
 
 	"github.com/Rohith-cyber2617/JS-Intel/internal/models"
+	"github.com/Rohith-cyber2617/JS-Intel/internal/scorer"
 )
 
 var patterns = map[string]*regexp.Regexp{
+
+	// Cloud Keys
 
 	"AWS_ACCESS_KEY": regexp.MustCompile(`AKIA[0-9A-Z]{16}`),
 
 	"GOOGLE_API_KEY": regexp.MustCompile(`AIza[0-9A-Za-z\-_]{35}`),
 
+	// Git
+
 	"GITHUB_TOKEN": regexp.MustCompile(`ghp_[A-Za-z0-9]{36}`),
+
+	"GITLAB_TOKEN": regexp.MustCompile(`glpat-[A-Za-z0-9\-_]+`),
+
+	// Tokens
 
 	"JWT": regexp.MustCompile(`eyJ[A-Za-z0-9_-]+\.[A-Za-z0-9_-]+\.[A-Za-z0-9_-]+`),
 
 	"BEARER_TOKEN": regexp.MustCompile(`Bearer\s+[A-Za-z0-9\-\._]+`),
 
-	"PRIVATE_KEY": regexp.MustCompile(`-----BEGIN PRIVATE KEY-----`),
+	// Databases
 
 	"MONGODB_URI": regexp.MustCompile(`mongodb://[^\s"'<>]+`),
 
 	"POSTGRES_URI": regexp.MustCompile(`postgres://[^\s"'<>]+`),
 
 	"MYSQL_URI": regexp.MustCompile(`mysql://[^\s"'<>]+`),
+
+	// Keys
+
+	"PRIVATE_KEY": regexp.MustCompile(`-----BEGIN PRIVATE KEY-----`),
+
+	// Firebase
+
+	"FIREBASE": regexp.MustCompile(`firebase`),
+
+	// Stripe
+
+	"STRIPE_SECRET": regexp.MustCompile(`sk_live_[A-Za-z0-9]+`),
+
+	// Twilio
+
+	"TWILIO": regexp.MustCompile(`twilio`),
+
+	// SendGrid
+
+	"SENDGRID": regexp.MustCompile(`sendgrid`),
+
+	// Admin
+
+	"ADMIN_ROUTE": regexp.MustCompile(`admin`),
+
+	"SUPERADMIN": regexp.MustCompile(`superadmin`),
+
+	"ADMIN_PANEL": regexp.MustCompile(`adminPanel`),
+
+	// Internal
+
+	"INTERNAL_API": regexp.MustCompile(`internal_api`),
+
+	"PRIVATE_API": regexp.MustCompile(`private_api`),
+
+	// GraphQL
+
+	"GRAPHQL": regexp.MustCompile(`graphql`),
+
+	"INTROSPECTION": regexp.MustCompile(`introspection`),
+
+	// Feature Flags
+
+	"FEATURE_FLAG": regexp.MustCompile(`featureFlag`),
+
+	"ENABLE_ADMIN": regexp.MustCompile(`enableAdmin`),
+
+	// Debug
+
+	"DEBUG_MODE": regexp.MustCompile(`debugMode`),
+
+	"DEV_ONLY": regexp.MustCompile(`devOnly`),
+
+	// Comments
+
+	"TODO": regexp.MustCompile(`TODO:`),
+
+	"FIXME": regexp.MustCompile(`FIXME:`),
+
+	"HACK": regexp.MustCompile(`HACK:`),
 }
 
 func FindSecrets(content string) []models.Finding {
@@ -40,7 +109,7 @@ func FindSecrets(content string) []models.Finding {
 			findings = append(findings, models.Finding{
 				Type:       findingType,
 				Value:      match,
-				Confidence: 9.0,
+				Confidence: scorer.CalculateConfidence(findingType),
 				Verified:   false,
 			})
 		}
