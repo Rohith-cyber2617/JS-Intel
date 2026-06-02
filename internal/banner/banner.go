b38edb480929b745657fa4a5d2086ba59194def0
Tony <rohithrachapudi96@gmail.com>
2026-06-02 10:23:34 +0530

    Create banner.go

---
+++ internal/banner/banner.go
@@ -0,0 +1,17 @@
+package banner
+
+const Banner = `
+         __  _____       ____       __       __
+        / / / ___/      /  _/___   / /____  / /
+       / /  \__ \       / // __ \ / __/ _ \/ /
+  __  / /  ___/ /    _/ // / / // /_/  __/  /
+ /___ _/ /_ _ _/    /___/_/ /_/ \__/\___/_/
+
+
+        JavaScript Intelligence Gathering Framework
+
+
+  Author    : TONY
+  LinkedIn  : linkedin.com/in/rohithrachapudi96
+  Portfolio : rohith-rachapudi-portfolio.vercel.app
+`
