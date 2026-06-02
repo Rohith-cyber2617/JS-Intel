4adae2520b18b2467be0c9f23dcead25fd256db4
Tony <rohithrachapudi96@gmail.com>
2026-06-02 10:24:01 +0530

    Create config package

---
+++ internal/config/config.go
@@ -0,0 +1,15 @@
+package config
+
+type Options struct {
+	URL         string
+	List        string
+	Output      string
+	Threads     int
+	Depth       int
+	Endpoints   bool
+	FoundOnly   bool
+	Verify      bool
+	RandomAgent bool
+	Silent      bool
+	Update      bool
+}
